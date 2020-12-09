b96a5d326cfc99637725c2999f48acb6ea33430c
Reyk Floeter <contact@reykfloeter.com>
2020-12-09 14:53:52 +0100

    Make Selectors public, add query! macro

--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,40 @@ mod error;
 mod query;
 mod selectors;
 
-pub use error::Error;
-pub use query::{Collection, Document, Element};
+pub use crate::{
+    error::Error,
+    query::{Collection, Document, Element},
+    selectors::Selectors,
+};
+
+/// `query!` macro to find elements.
+///
+/// This macro is a helper for the `find` method.
+///
+/// # Examples
+///
+/// ```rust
+/// use wasm_bindgen::prelude::*;
+/// use web_sys_query::{query, Error};
+///
+/// #[wasm_bindgen]
+/// pub fn hello() {
+///     query!("body").unwrap().set_text("Hello, World!");
+/// }
+/// # fn main() {}
+/// ```
+#[macro_export]
+macro_rules! query {
+    ($obj:ident, $selector:expr) => {
+        match AsRef::<str>::as_ref(&$selector).parse() {
+            Ok(selectors) => $obj.find(&selectors),
+            Err(err) => Err(err),
+        }
+    };
+    ($selector:expr) => {
+        match web_sys_query::Document::new() {
+            Ok(document) => query!(document, $selector),
+            Err(err) => Err(err),
+        }
+    };
+}
